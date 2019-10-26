fb4bbed2a829065d7ac17ee547f6334e18a05e48
thomasneo <thomaslossio@hotmail.com>
2019-10-25 22:24:42 -0300

    Routes initiliazed, Input and Button Components created and styled, Root Import set up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {View} from 'react-native';
+
+import Routes from './routes';
 
 import './config/ReactotronConfig';
 
 export default function App() {
-  return <View />;
+  return <Routes />;
 }
