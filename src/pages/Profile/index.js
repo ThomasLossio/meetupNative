8d0b76ee07424de7019373b6de0d27cd79f85e92
thomasneo <thomaslossio@hotmail.com>
2019-10-27 03:02:17 -0300

    Dashboard list done. It's already possible to subscribe in a meetup.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {Text} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import Header from '~/components/Header';
 import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 export default function Profile() {
-  return <Background />;
+  return (
+    <>
+      <Header />
+      <Background />
+    </>
+  );
 }
 
 Profile.navigationOptions = {
